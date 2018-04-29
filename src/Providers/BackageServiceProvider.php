7ecbc95143dea4b358efe771eb55bed7ad620713
eslam esmail <eslam.esmail@olahub.com>
2018-04-29 11:08:31 +0200

    update

--- src/Providers/BackageServiceProvider.php
+++ src/Providers/BackageServiceProvider.php
@@ -3,13 +3,14 @@
 namespace OlaHub\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use OlaHub\Models\OlahubAdminModel;
+//use OlaHub\Models\OlahubAdminModel;
 
-class AppServiceProvider extends ServiceProvider
+class BackageServiceProvider extends ServiceProvider
 {
     
     public function boot(){
-        OlahubAdminModel::observe("OlaHub\\Observers\\OlaHubAdminObserve");
+//        OlahubAdminModel::observe("OlaHub\\Observers\\OlaHubAdminObserve");
+        echo 'here done';
     }
     /**
      * Register any application services.
